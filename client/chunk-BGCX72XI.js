a26d69f305c1cbcabbaf8abfb7bb4d1b8e442db9
Parth Dhankecha <pmd3290@gmail.com>
2026-03-25 11:23:49 +0530

    Build upload

---
+++ client/chunk-BGCX72XI.js
@@ -0,0 +1,1 @@
+import{Ta as r,Xa as t,cb as o,o as e}from"./chunk-XN4J7IFH.js";var h=(u,a)=>{let i=e(r);if(!e(o).utils.isAuthenticated){let n=a.url.startsWith(`/${t.ADMIN.BASE}`);return i.navigateByUrl(n?t.ADMIN.getFullRoute(t.ADMIN.LOGIN):`/${t.AUTH.BASE}`),!1}return!0};export{h as a};

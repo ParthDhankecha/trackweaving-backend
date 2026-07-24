6c2452c465460491d6683f380ef4f0ec122711c9
ronakvirpara <ronakvirpara888@gmail.com>
2026-07-24 11:38:25 +0530

    add new reports, separate report service for device

--- src/api/routes/device/machineLogs.js
+++ src/api/routes/device/machineLogs.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const machineLogsController = require('../../controllers/device/machineLogsController');
 const isAuth = require('../../middleware/auth');
 
+
+router.get('/qualities', isAuth, machineLogsController.getQualityList);
+
 router.post('/list', isAuth, machineLogsController.getList);
 
 

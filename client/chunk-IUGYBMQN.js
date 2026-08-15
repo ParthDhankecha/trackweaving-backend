3b6c37313688e8a441915ad33dffec8a6fc3f833
ronakvirpara <ronakvirpara888@gmail.com>
2026-08-15 13:02:22 +0530

    Build upload

---
+++ client/chunk-IUGYBMQN.js
@@ -0,0 +1,1 @@
+var a={USER_NAME:{REGEXP:/^[a-zA-Z][a-zA-Z0-9_]{5,}$/,MESSAGE:"Invalid user name. It should start with a letter and contain only letters, numbers, and underscores and be at least 6 characters long."}},t=a;export{t as a};

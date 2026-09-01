820792255fa91c1418266a66b645b6a013deab7b
Parth Dhankecha <pmd3290@gmail.com>
2026-09-01 17:24:37 +0530

    Change regarding alarm

--- src/api/services/reportService.device.js
+++ src/api/services/reportService.device.js
@@ -174,6 +174,10 @@ function buildProductionReportData(reportData, machines, workspace, options = {}
         };
         delete data.stopsCount;
 
+        data.alarmsActive = Array.isArray(data.alarmsActive)
+            ? data.alarmsActive.filter(Boolean)
+            : [];
+
         if (data.machineType === 'rapier') {
             const runTime = data.runTime?.split(':') || [];
             if (runTime.length > 1) {
@@ -261,7 +265,6 @@ const LOG_PROJECTION = {
     picksTotal: false,
     setPicks: false,
     stop: false,
-    alarmsActive: false,
     loomStateCode: false,
     isDeleted: false
 };
--- src/api/services/reportService.js
+++ src/api/services/reportService.js
@@ -193,6 +193,10 @@ function buildProductionReportData(reportData, machines, workspace, options = {}
         };
         delete data.stopsCount;
 
+        data.alarmsActive = Array.isArray(data.alarmsActive)
+            ? data.alarmsActive.filter(Boolean)
+            : [];
+
         if (data.machineType === 'rapier') {
             const runTime = data.runTime?.split(':') || [];
             if (runTime.length > 1) {
@@ -280,7 +284,6 @@ const LOG_PROJECTION = {
     picksTotal: false,
     setPicks: false,
     stop: false,
-    alarmsActive: false,
     loomStateCode: false,
     isDeleted: false
 };
